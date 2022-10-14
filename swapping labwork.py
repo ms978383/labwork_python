a2965c425d6f5c847891dec19f253225137d1981
ms978383 <smaheshparmar3026@gmail.com>
2022-10-14 19:45:34 +0530

    Create swapping labwork.py

---
+++ swapping labwork.py
@@ -0,0 +1,13 @@
+p=int(input("enter value of p : "))
+q=int(input("enter value of q : "))
+r=int(input("enter value of r : "))
+s=int(input("enter value of s : "))
+
+print("before swaping\n")
+p,q,r,s=q,r,s,p
+print("after swapping")
+print("value p:",p)
+print("value q:",q)
+print("value r:",r)
+print("value s:",s)
+
