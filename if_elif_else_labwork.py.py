cba410741c84069c80a2125598412dc88e2a9a11
ms978383 <smaheshparmar3026@gmail.com>
2022-10-14 19:45:23 +0530

    Create if_elif_else_labwork.py.py

---
+++ if_elif_else_labwork.py.py
@@ -0,0 +1,12 @@
+
+age=int(input("please enter driver's age:"))
+
+if age<18:
+    print("you can't drive.")
+elif age==18:
+    print(" i'm not satisfied , please come to rto office and check.")
+else:
+    print("safe drive.")
+    
+
+
